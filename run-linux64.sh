74ab14b3170ef902467dd977be33e2b8d2dd62d8
Vanya <vanya.braslavets@gmail.com>
2024-05-22 12:06:09 +0300

    removed build scripts (not needed anymore)

--- run-linux64.sh
+++
@@ -1,5 +0,0 @@
-dotnet run
-nasm -o Resources/test.o -f elf64 Resources/test.S
-gcc -fPIC Resources/test.o --entry=_start -nostartfiles -o Resources/test
-./Resources/test
-echo $?
--- run-win64.bat
+++
@@ -1,3 +0,0 @@
-dotnet run run Resources/test.txt
-
-:: look into this: -fomit-frame-pointer
